df62125f58ab829b94be12b046fcfe5055b32abf
Arslan Ahmad <ars.ahm6@gmail.com>
2025-03-27 16:15:44 +0500

    basic engine.

---
+++ XmlReplacer.Engine/Replacer.cs
@@ -0,0 +1,20 @@
+﻿namespace XmlReplacer.Engine;
+
+public class Replacer
+{
+    public Replacer(string rootPath, bool isRecursive)
+    {
+        if (!Path.Exists(rootPath))
+        {
+            throw new Exception($"{rootPath} does not exist.");
+        }
+
+        RootPath = rootPath;
+        IsRecursive = isRecursive;
+    }
+
+    public string RootPath { get; }
+    public bool IsRecursive { get; }
+
+    public void SetAttributeValue(string elementName, string attributeName, string newValue, Action<Exception> onError) => XmlHelper.UpdateXmlAttributes(RootPath, elementName, attributeName, newValue, onError);
+}
---
+++ XmlReplacer.Engine/XmlHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml;
+
+namespace XmlReplacer.Engine;
+
+static class XmlHelper
+{
+    public static void UpdateXmlAttributes(string rootPath, string elementName, string attributeName, string newValue, Action<Exception> onError)
+    {
+        foreach (var filePath in Directory.EnumerateFiles(rootPath, "*.xml", SearchOption.AllDirectories))
+        {
+            try
+            {
+                UpdateXmlFile(filePath, elementName, attributeName, newValue, onError);
+            }
+            catch (Exception ex)
+            {
+                onError(ex);
+            }
+        }
+    }
+
+    public static void UpdateXmlFile(string filePath, string elementName, string attributeName, string newValue, Action<Exception> onError)
+    {
+        try
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(filePath);
+
+            XmlNodeList elements = xmlDocument.GetElementsByTagName(elementName);
+
+            foreach (XmlElement element in elements)
+            {
+                if (element.HasAttribute(attributeName))
+                {
+                    element.SetAttribute(attributeName, newValue);
+                }
+            }
+
+            xmlDocument.Save(filePath);
+        }
+        catch (Exception ex)
+        {
+            onError(ex);
+        }
+    }
+}
