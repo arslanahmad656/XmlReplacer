91f25f48043d52c81ad7c9747d431d3abf319094
Arslan Ahmad <ars.ahm6@gmail.com>
2025-03-27 16:28:55 +0500

    added the GUI.

---
+++ XmlSearcher.Cli/Program.cs
@@ -0,0 +1,29 @@
+﻿using XmlReplacer.Engine;
+
+string rootPath = args[0];
+string elementName = args[1];
+string attributeName = args[2];
+string newValue = args[3];
+
+if (string.IsNullOrEmpty(rootPath) || string.IsNullOrEmpty(elementName) ||
+            string.IsNullOrEmpty(attributeName) || string.IsNullOrEmpty(newValue))
+{
+    Console.WriteLine("Please provide all required arguments: rootPath, elementName, attributeName, newValue.");
+    return;
+}
+
+try
+{
+    var replacer = new Replacer(rootPath);
+    replacer.SetAttributeValue(elementName, attributeName, newValue, ex =>
+    {
+        var color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Error detected: {ex.GetType().FullName}: {ex.Message}");
+        Console.ForegroundColor = color;
+    });
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
